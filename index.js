e54a56de23ae1e15f31cea81ffc53660c4ae44b3
David Durán <dduran@scotiabank.com.mx>
2022-01-13 21:42:33 -0600

    feat: users services

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const cors = require('cors');
+const routerApi = require('./routes');
+
 const {
   logErrors,
   errorHandler,
   boomErrorHandler,
 } = require('./middlewares/error.handler');
-const jsonParser = bodyParser.json();
+
 const app = express();
+const port = process.env.PORT || 3000;
 
-const port = 3000;
-app.use(jsonParser);
+app.use(express.json());
 
-const routerApi = require('./routes');
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  },
+};
+app.use(cors(options));
 routerApi(app);
 
-//errors middlewares
+//middlewares
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log('Server started', port);
+  console.log('Mi port' + port);
 });
